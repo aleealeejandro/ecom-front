bf56948ef4e3bb74ed89f1c325ddd0910a548a2d
alexanderpadilla <alexander.padilla@g.austincc.edu>
2020-08-26 12:25:03 -0500

    clean up code

--- src/admin/ManageCategories.js
+++ src/admin/ManageCategories.js
@@ -80,36 +80,6 @@ export default ManageCategories;
 
 
 
-// {
-// 	<div className="row">
-// 		<div className='col-5 text-center'>
-// 			<h2 className="text-center">
-// 				Total Categories: {categories.length}
-// 			</h2>
-// 			<ul className="list-group">
-// 				{categories.map((category) => (
-// 					<li 
-// 						key={category._id} 
-// 						className="list-group-item d-flex  align-items-center"
-// 					>
-// 						<strong>{category.name}</strong>
-// 						<Link to={`/admin/category/update/${category._id}`}>
-// 							<span className="badge badge-warning badge-pill">
-// 								Update
-// 							</span>
-// 						</Link>
-// 						<span onClick={() => destroyCategory(category._id)} className="badge badge-danger badge-pill">
-// 							Delete
-// 						</span>
-// 					</li>
-// 				))}
-// 			</ul>
-// 		</div>
-// 	</div>
-// }
-
-
-
 
 
 
